a553b61c36d911a3cfc6a2a99f044be5ab7c7935
shohan <dev.shohanur@gmail.com>
2023-08-14 01:23:29 +0600

    view update and delete added

--- src/functions.js
+++ src/functions.js
@@ -4,7 +4,7 @@
  * @param {*} data 
  */
 const sendDataLS = (key, data) => {
-    localStorage.setItem(key, JSON.stringify(data))
+  localStorage.setItem(key, JSON.stringify(data))
 }
 
 /**
@@ -13,10 +13,10 @@ const sendDataLS = (key, data) => {
  * @returns 
  */
 const getDataLS = (key) => {
-    if (localStorage.getItem(key)) {
-        return JSON.parse(localStorage.getItem(key))
-    }
-    return []
+  if (localStorage.getItem(key)) {
+    return JSON.parse(localStorage.getItem(key))
+  }
+  return []
 }
 
 /**
@@ -27,8 +27,8 @@ const getDataLS = (key) => {
 
 
 const isNum = (num) => {
-    pattern = /^[0-9]{3,}$/
-    return pattern.test(num)
+  pattern = /^[0-9]{3,}$/
+  return pattern.test(num)
 }
 
 
@@ -39,7 +39,7 @@ const isNum = (num) => {
  * @returns 
  */
 const createAlert = (msg, type = "danger") => {
-    return `       <p class="alert alert-${type} alert-dismissible fade show" role="alert">
+  return `<p class="alert alert-${type} alert-dismissible fade show" role="alert">
 ${msg}
 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </p>`
@@ -52,33 +52,49 @@ ${msg}
  * @returns 
  */
 function timeAgo(date) {
-    const currentDate = new Date();
-    const timestamp = date instanceof Date ? date.getTime() : new Date(date).getTime();
-    const elapsed = currentDate - timestamp;
-  
-    if (elapsed < 1000) {
-      return 'just now';
-    } else if (elapsed < 60000) {
-      const seconds = Math.floor(elapsed / 1000);
-      return `${seconds} ${seconds === 1 ? 'second' : 'seconds'} ago`;
-    } else if (elapsed < 3600000) {
-      const minutes = Math.floor(elapsed / 60000);
-      return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
-    } else if (elapsed < 86400000) {
-      const hours = Math.floor(elapsed / 3600000);
-      return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
-    } else if (elapsed < 2592000000) {
-      const days = Math.floor(elapsed / 86400000);
-      return `${days} ${days === 1 ? 'day' : 'days'} ago`;
-    } else if (elapsed < 31536000000) {
-      const months = Math.floor(elapsed / 2592000000);
-      return `${months} ${months === 1 ? 'month' : 'months'} ago`;
-    } else {
-      const years = Math.floor(elapsed / 31536000000);
-      return `${years} ${years === 1 ? 'year' : 'years'} ago`;
-    }
+  const currentDate = new Date();
+  const timestamp = date instanceof Date ? date.getTime() : new Date(date).getTime();
+  const elapsed = currentDate - timestamp;
+
+  if (elapsed < 1000) {
+    return 'just now';
+  } else if (elapsed < 60000) {
+    const seconds = Math.floor(elapsed / 1000);
+    return `${seconds} ${seconds === 1 ? 'second' : 'seconds'} ago`;
+  } else if (elapsed < 3600000) {
+    const minutes = Math.floor(elapsed / 60000);
+    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+  } else if (elapsed < 86400000) {
+    const hours = Math.floor(elapsed / 3600000);
+    return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+  } else if (elapsed < 2592000000) {
+    const days = Math.floor(elapsed / 86400000);
+    return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+  } else if (elapsed < 31536000000) {
+    const months = Math.floor(elapsed / 2592000000);
+    return `${months} ${months === 1 ? 'month' : 'months'} ago`;
+  } else {
+    const years = Math.floor(elapsed / 31536000000);
+    return `${years} ${years === 1 ? 'year' : 'years'} ago`;
   }
+}
 
-  
 
+
+/**
+ * unique id
+ */
+
+const randomUniqueId = (length = 20) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const idArray = [];
   
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    const randomChar = characters.charAt(randomIndex);
+    idArray.push(randomChar);
+  }
+  
+  const uniqueId = idArray.join('');
+  return uniqueId;
+}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,25 @@
 const create_student_form = document.querySelector("#create_student_form");
 const alertMsg = document.querySelector(".msg");
 const all_student_list = document.querySelector(".all_student_list");
+const singleStudentInfo = document.querySelector(".singleStudentInfo");
+const edit_student_form = document.querySelector("#edit_student_form");
+const msg_edit = document.querySelector(".msg_edit");
 
 //show data
 
 const getStudent = () => {
     const students = getDataLS("students")
-    
+
     console.log(students);
 
     let content = ""
 
-    students.map((student, index) => {
-        content += `
+    if (students.length > 0) {
+
+        students.map((student, index) => {
+            content += `
         <tr>
-        <th scope="row">${index+1}</th>
+        <th scope="row">${index + 1}</th>
         <td>
           <img style="width: 60px; height: 60px; object-fit: cover;"
             src="${student.photo}"
@@ -28,22 +33,111 @@ const getStudent = () => {
           <button type="button" class="btn btn-primary">Add Result</button>
         </td>
         <td>
-          <button class="btn btn-sm bg-info text-light"><i class="fa-solid fa-eye"></i></button>
-          <button class="btn btn-sm bg-warning text-light"><i class="fa-solid fa-edit"></i></button>
-          <button class="btn btn-sm bg-danger text-light"><i class="fa-solid fa-trash"></i></button>
+          <button class="btn btn-sm bg-info text-light" data-bs-toggle="modal" data-bs-target="#showStudentInfo" onClick = showStudentData('${student.id}')><i class="fa-solid fa-eye" ></i></button>
+
+          <button class="btn btn-sm bg-warning text-light"  data-bs-toggle="modal" data-bs-target="#editStudentInfo" onClick = "updateStudent('${student.id}')"><i class="fa-solid fa-edit"></i></button>
+          <button class="btn btn-sm bg-danger text-light" onClick = "deleteStudent('${student.roll}')" ><i class="fa-solid fa-trash"></i></button>
         </td>
       </tr>
         `
-    })
+        })
+
+    } else {
+        content = `
+        <tr>
+        <td colspan= "8" class = "text-center" >No data found</td>
+        </tr>
+        `
+    }
     all_student_list.innerHTML = content
+
 }
 getStudent()
+
+
+// delete data
+const deleteStudent = (roll) => {
+
+    const deleteConfirm = confirm(`Are you sure, Your data will be delete`)
+
+    if (deleteConfirm) {
+        const oldStudent = getDataLS("students")
+        const updatedStudate = oldStudent.filter((data) => data.roll != roll);
+        sendDataLS("students", updatedStudate);
+        getStudent()
+    } else {
+        alert(`Your data is safe`)
+    }
+
+}
+
+// view data
+const showStudentData = (id) => {
+    const allStudents = getDataLS("students");
+    const singleStudent = allStudents.find(item => item.id === id)
+
+    singleStudentInfo.innerHTML = `
+    <img class="w-25"
+    src="${singleStudent.photo}"
+    alt="" srcset="" />
+  <div class="details">
+    <h3><span>Name: </span>${singleStudent.name}</h3>
+    <p><span>Roll: <span>${singleStudent.roll} </span> | <span>Reg: <span>${singleStudent.reg}</p>
+  </div>
+    `
+}
+// edit data
+const updateStudent = (id) => {
+    const oldStudent = getDataLS("students")
+    const newData = oldStudent.find((item) => item.id === id)
+    edit_student_form.querySelector("input[name='id']").value = newData.id
+    edit_student_form.querySelector("input[name='name']").value = newData.name
+    edit_student_form.querySelector("input[name='roll']").value = newData.roll
+    edit_student_form.querySelector("input[name='reg']").value = newData.reg
+    edit_student_form.querySelector("input[name='photo']").value = newData.photo
+    edit_student_form.querySelector("img").setAttribute("src", newData.photo)
+
+}
+
+edit_student_form.onsubmit = (e) => {
+
+
+
+    e.preventDefault()
+
+    const form_data = new FormData(e.target);
+    const data = Object.fromEntries(form_data.entries())
+
+
+
+    if (!data.name || !data.roll || !data.reg) {
+        msg_edit.innerHTML = createAlert("Fill all input")
+    } else if (!isNum(data.roll)) {
+        msg_edit.innerHTML = createAlert("Invalid Roll")
+
+    } else if (!isNum(data.reg)) {
+        msg_edit.innerHTML = createAlert("Invalid Reg")
+
+    } else {
+
+        const getOldData = getDataLS("students")
+        getOldData[getOldData.findIndex(item => item.id === data.id)] = {
+            ...getOldData[getOldData.findIndex(item => item.id === data.id)],
+            ...data
+        }
+        sendDataLS("students", getOldData)
+        getStudent()
+        msg_edit.innerHTML = createAlert(` Updated`, "success")
+    }
+}
+
 
 create_student_form.addEventListener("submit", function (e) {
     e.preventDefault()
 
     const form_data = new FormData(e.target);
     const data = Object.fromEntries(form_data.entries())
+
 
 
     if (!data.name || !data.roll || !data.reg) {
@@ -55,12 +149,26 @@ create_student_form.addEventListener("submit", function (e) {
         alertMsg.innerHTML = createAlert("Invalid Reg")
 
     } else {
-
         const oldStudent = getDataLS("students")
+
+
+        if (oldStudent.some(item => item.roll === data.roll)) {
+            alertMsg.innerHTML = createAlert("Roll already registered")
+            return;
+        }
+
+
+        if (oldStudent.some(item => item.reg === data.reg)) {
+            alertMsg.innerHTML = createAlert("Reg already registered")
+            return;
+        }
+
         oldStudent.push({
             ...data,
             result: null,
-            createdTime: Date.now()
+            createdTime: Date.now(),
+            id: randomUniqueId()
+
         })
 
         sendDataLS("students", oldStudent)
